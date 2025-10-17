86599569470fa8b3bd75a30da764e5cc219052f5
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-10-17 10:13:57 -0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,44 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "1c40cf6f-469e-40fa-b4b5-52a40b5abf60",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Roll a dice!\n"
+     ]
+    }
+   ],
+   "source": [
+    "msg = \"Roll a dice!\"\n",
+    "print(msg)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
